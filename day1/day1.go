fddcafb79ca983ac029d560d04959ca0bb7bc1bd
abhilash26 <srivastava.abhilash26@gmail.com>
2025-12-19 19:28:40 +0530

    feat: add day1

--- day1/day1.go
+++ day1/day1.go
@@ -4,36 +4,20 @@ import (
 	"strconv"
 )
 
-
-func getDialRotation(current int, line string) int {
-	rotationType := line[0]
-	rotationValue, err := strconv.Atoi(line[1:])
-	if err != nil {
-		return 0
-	}
-	switch rotationType {
-	case 'L':
-		if current - rotationValue < 0 {	
-			return 100 - (rotationValue - current)
-		} else {
-			return current - rotationValue
-		}
-	case 'R':
-		if current + rotationValue > 99 {
-			return current + rotationValue - 100
-		} else {
-			return current + rotationValue
-		}
-	}
-	return 0
-}
-
-
 func Part1(lines []string) int {
 	countZero := 0
 	current := 50
+
 	for _, line := range lines {
-		current = getDialRotation(current, line)
+		rotationDirection := line[0]
+		rotationVal, _ := strconv.Atoi(line[1:])
+		switch rotationDirection {
+		case 'L':
+			current -= rotationVal
+		case 'R':
+			current += rotationVal
+		}
+		current %= 100
 		if current == 0 {
 			countZero++
 		}
@@ -42,5 +26,36 @@ func Part1(lines []string) int {
 }
 
 func Part2(lines []string) int {
-	return 0
+	countZero := 0
+	current := 50
+	distancetoZero := 0
+
+	for _, line := range lines {
+		rotationDirection := line[0]
+		rotationVal, _ := strconv.Atoi(line[1:])
+
+
+		if rotationDirection == 'L' {
+			distancetoZero = 100 - current
+			current += rotationVal
+		} else {
+			distancetoZero = current
+			current -= rotationVal
+		}
+
+		// Update countZero
+		if distancetoZero == 0 {
+			countZero += rotationVal / 100
+		} else if rotationVal >= distancetoZero {
+			countZero += 1 + (rotationVal-distancetoZero)/100
+		}
+
+		// Update current
+		current %= 100
+		if current < 0 {
+			current += 100
+		}
+
+	}
+	return countZero
 }
